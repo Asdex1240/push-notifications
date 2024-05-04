431bc4321efa7d6ae8a0af5f76f8f1c953e9d7d7
Juan Jose Moreno Barron <jose.morenobrn@uanl.edu.mx>
2024-05-04 12:29:19 -0600

    Notificaciones push para Android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'dev.asdex.notifications',
   appName: 'push-notifications',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
+  }
 };
 
 export default config;
