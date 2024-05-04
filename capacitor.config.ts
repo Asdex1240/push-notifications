645974b199d9b378f79f76619918c2a688e1dc63
Juan Jose Moreno Barron <jose.morenobrn@uanl.edu.mx>
2024-05-04 11:48:12 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'push-notifications',
+  webDir: 'www'
+};
+
+export default config;
